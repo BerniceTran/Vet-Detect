fcee52a1fdb0d70462ca7d687f5f36cc98a437d3
Bernice Tran <bernicetran94@gmail.com>
2021-10-06 18:43:27 -0400

    Edit server.js

--- backend/server.js
+++ backend/server.js
@@ -140,10 +140,10 @@ app.use((err, req, res, next) => {
     res.status(500).send({message: err.message});
 });
 
-app.use(express.static(path.join(__dirname, 'backend/frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'backend/frontend/build/index.html'));
+  res.sendFile(path.join(__dirname + 'frontend/build/index.html'));
 });
 
 const port = process.env.PORT || 3001;
